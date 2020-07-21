9df94435c1f4324dc7856b9497d47e9b349d8ca3
Zac Scott <scottzach1@myvuw.ac.nz>
2020-07-22 01:10:44 +1200

    Pulls version data from TransitFeed API for Wellington
    In this commit, we simply pull all version data for Wellington
    Metlink service using the TransitFeed API. Currently this data
    is just being rendered to the main canvas of the ServicesTab.tsx.

--- src/pages/ServicesTab.tsx
+++ src/pages/ServicesTab.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import React, {useState} from 'react';
+import {IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './ServicesTab.css';
 
 const ServicesTab: React.FC = () => {
+    const [ versionData, setVersionData ] = useState<string>()
+
+    async function getVersionData() {
+        const url = 'https://api.transitfeeds.com/v1/getFeedVersions?key=16d2be4b-c141-4e14-a4bb-c40b8f7dcb7b&feed=metlink%2F22&page=1&limit=10&err=1&warn=1';
+        const result = await fetch(url)
+
+        setVersionData(await result.text())
+    }
+
+    getVersionData().then()
+
     return (
         <IonPage>
             <IonHeader>
@@ -17,7 +28,13 @@ const ServicesTab: React.FC = () => {
                         <IonTitle size="large">Services</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <ExploreContainer name="Bus Services"/>
+                {/*<ExploreContainer name="Bus Services"/>*/}
+                {versionData && (
+                    <IonLabel>
+                        <p>Version Data:</p>
+                        <p>{versionData}</p>
+                    </IonLabel>
+                )}
             </IonContent>
         </IonPage>
     );
