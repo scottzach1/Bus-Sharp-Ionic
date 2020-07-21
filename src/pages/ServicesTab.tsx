5160a6669c54cc958eef7f5e7425478b2d19b521
Zac Scott <scottzach1@myvuw.ac.nz>
2020-07-22 01:18:39 +1200

    Parses latest version value from response Json.
    In this commit, we parse the version id from the most recent
    result within the received versions data. This value is then
    outputted to the ServicesTab.tsx.

--- src/pages/ServicesTab.tsx
+++ src/pages/ServicesTab.tsx
@@ -10,7 +10,9 @@ const ServicesTab: React.FC = () => {
         const url = 'https://api.transitfeeds.com/v1/getFeedVersions?key=16d2be4b-c141-4e14-a4bb-c40b8f7dcb7b&feed=metlink%2F22&page=1&limit=10&err=1&warn=1';
         const result = await fetch(url)
 
-        setVersionData(await result.text())
+        const json = await result.json()
+
+        setVersionData(json.results.versions[0].id)
     }
 
     getVersionData().then()
@@ -28,11 +30,12 @@ const ServicesTab: React.FC = () => {
                         <IonTitle size="large">Services</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                {/*<ExploreContainer name="Bus Services"/>*/}
+
+                <ExploreContainer name="Bus Services"/>
+
                 {versionData && (
                     <IonLabel>
-                        <p>Version Data:</p>
-                        <p>{versionData}</p>
+                        <p>Latest Version Data: {versionData}</p>
                     </IonLabel>
                 )}
             </IonContent>
