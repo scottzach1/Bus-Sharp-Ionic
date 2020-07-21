03e9a737077a2f8feb711d2b4d0de36782e2e316
Zac Scott <scottzach1@myvuw.ac.nz>
2020-07-22 10:09:30 +1200

    Slightly tidier implementation of previous getData() method

--- src/pages/ServicesTab.tsx
+++ src/pages/ServicesTab.tsx
@@ -8,9 +8,7 @@ const ServicesTab: React.FC = () => {
 
     async function getVersionData() {
         const url = 'https://api.transitfeeds.com/v1/getFeedVersions?key=16d2be4b-c141-4e14-a4bb-c40b8f7dcb7b&feed=metlink%2F22&page=1&limit=10&err=1&warn=1';
-        const result = await fetch(url)
-
-        const json = await result.json()
+        const json = await (await fetch(url)).json()
 
         setVersionData(json.results.versions[0].id)
     }
