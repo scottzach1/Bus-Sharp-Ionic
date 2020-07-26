2ab1d4ece3d2e923d65bc5d262e1f3c6fbda1184
Zac Scott <scottzach1@myvuw.ac.nz>
2020-07-26 20:48:36 +1200

    Renders all route positions on the map as markers.
    In this commit, we parse in the route data from the
    unofficial Metlink API and render the points onto
    the map as markers. In this commit we only render
    the first route for a given service and these are
    rendered as markers instead of paths.

--- src/components/metlink/MetlinkServiceView.tsx
+++ src/components/metlink/MetlinkServiceView.tsx
@@ -1,10 +1,5 @@
 import React, {FC, useState} from 'react';
-import {
-    GoogleMap,
-    useLoadScript,
-    Marker,
-    InfoWindow,
-} from "@react-google-maps/api";
+import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
 
 interface Props {
     serviceCode: string;
@@ -26,8 +21,10 @@ const options = {
 }
 
 const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
-    const [serviceData, setServiceData] = useState<any>();
-    const [markers, setMarkers] = React.useState<Array<Marker>>([]);
+    const [serviceData, setServiceData] = useState<any | null>(null);
+    // const [isDataLoaded, setIsDataLoaded] = useState<boolean>(false);
+    const [markers, setMarkers] = React.useState<any[]>([]);
+    const [errorMessage, setErrorMessage] = useState<string>()
 
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -38,21 +35,80 @@ const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
     if (loadError) return (<p>"Error: load error"</p>)
     if (!isLoaded) return (<p>"Error: not loaded"</p>)
 
+    async function getRouteData() {
+        if (serviceData) return;
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://www.metlink.org.nz/api/v1/ServiceMap/";
+
+        fetch(proxy + url + serviceCode)
+            .then(resp => {
+                if (!resp.ok) setErrorMessage(resp.statusText);
+                else Promise.resolve(resp.json())
+                    .then(data => {
+                        // console.log(data);
+                        setServiceData(data);
+                        generateMapMarkers(data);
+                    });
+            });
+    }
+
+    function generateMapMarkers(mapData: any) {
+        let parsedMarkers: any[] = [];
+
+        console.log(mapData);
+
+        let counter: number = 0;
+        for (const point of mapData.RouteMaps[0].Path) {
+            const location: string[] = point.split(",");
+
+            parsedMarkers.push(
+                <Marker
+                    position={{
+                        lat: parseFloat(location[0]),
+                        lng: parseFloat(location[1]),
+                    }}
+                    key={counter++ + ':' + location[0] + ',' + location[1]}
+                    options={}
+                />
+            );
+        }
+
+        setMarkers(parsedMarkers);
+    }
+
+// markers.push(
+//     <Marker
+//         key={latlng[0] + '-' + latlng[1]}
+//         position={{
+//             lat: latlng[0],
+//             lng: latlng[1],
+//         }}
+//     />
+// );
+
+// return markers;
+// }
+
+    getRouteData().then();
+
     return (
         <div>
             <p>Service Code: {serviceCode}</p>
-            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} options={options} onClick={event => {
-                console.log('hellooo!');
-            }}>
-                {/*{markers.map(marker => (*/}
-                {/*    <Marker*/}
-                {/*        key={}*/}
-                {/*        position={marker. }*/}
-                {/*    />*/}
-                {/*))}*/}
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+                options={options}
+                onClick={event => {
+                    console.log(event);
+                }}
+            >
+                {markers && (
+                    markers
+                )}
             </GoogleMap>
         </div>
-    )
-};
+    );
+}
 
 export default MetlinkServiceView;
