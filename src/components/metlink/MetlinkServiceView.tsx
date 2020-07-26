97811bbfe740bfb14c2256f2c7f8875e33b36cbf
Zac Scott <scottzach1@myvuw.ac.nz>
2020-07-26 22:25:43 +1200

    Tidies and optimises Map code.
    In this commit, we focus on tidying and optimising map code.
    We ensure that the map data is only calculated once after the
    data is loaded.

--- src/components/metlink/MetlinkServiceView.tsx
+++ src/components/metlink/MetlinkServiceView.tsx
@@ -8,22 +8,21 @@ interface Props {
 const libraries = ["places"];
 const mapContainerStyle = {
     width: '100vw',
-    height: '100vh',
+    height: '100vh', // Need to figure out how to get 100% of max container height - no zoom!
 };
 const center = {
     lat: -41.286461,
     lng: 174.776230,
 }
 const options = {
-    disableDefaultUI: true,
+    // disableDefaultUI: true, // Commented for street view.
     zoomControl: true,
     mapTypeControl: true,
 }
 
 const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
-    const [serviceData, setServiceData] = useState<any | null>(null);
-    // const [isDataLoaded, setIsDataLoaded] = useState<boolean>(false);
-    const [routePath, setRoutePath] = React.useState<any[]>();
+    const [dataIsLoaded, setDataIsLoaded] = useState<boolean>(false);
+    const [routePath, setRoutePath] = React.useState<any[]>([]);
     const [stopMarkers, setStopMarkers] = React.useState<any[]>([])
     const [errorMessage, setErrorMessage] = useState<string>()
 
@@ -37,7 +36,7 @@ const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
     if (!isLoaded) return (<p>"Error: not loaded"</p>)
 
     async function getRouteData() {
-        if (serviceData) return;
+        if (dataIsLoaded) return;
         const proxy = "https://cors-anywhere.herokuapp.com/";
         const url = "https://www.metlink.org.nz/api/v1/ServiceMap/";
 
@@ -46,52 +45,60 @@ const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
                 if (!resp.ok) setErrorMessage(resp.statusText);
                 else Promise.resolve(resp.json())
                     .then(data => {
-                        // console.log(data);
-                        setServiceData(data);
+                        setDataIsLoaded(true);
                         generateMapRoute(data);
                     });
             });
     }
 
     function generateMapRoute(mapData: any) {
+        // Buffer for calculated map elements.
         let parsedRoutes: any[] = [];
         let parsedMarkers: any[] = [];
 
-        for (const route of mapData.RouteMaps) {
-            let parsedRoute: any[] = [];
+        if (routePath.length === 0) {
+            // Pre-calculate values, update prop after.
+            let counter: number = 0;
+            for (const route of mapData.RouteMaps) {
+                let parsedRoute: any[] = [];
 
-            for (const point of route.Path) {
-                const location: string[] = point.split(",");
+                for (const point of route.Path) {
+                    const location: string[] = point.split(",");
 
-                parsedRoute.push({
-                    lat: parseFloat(location[0]),
-                    lng: parseFloat(location[1]),
-                });
-            }
-
-            parsedRoutes.push(
-                <Polyline key={"route-" + serviceCode} path={parsedRoute} options={{strokeColor: "#e076b4"}}/>
-            );
-        }
-
-
-        for (const point of mapData.StopLocations) {
-            const location: string[] = point.LatLng.split(",");
-            const stopCode: string = point.Sms;
-
-            parsedMarkers.push(
-                <Marker
-                    position={{
+                    parsedRoute.push({
                         lat: parseFloat(location[0]),
                         lng: parseFloat(location[1]),
-                    }}
-                    key={'stop-' + stopCode}
-                />
-            );
+                    });
+                }
+
+                parsedRoutes.push(
+                    <Polyline key={"route:" + serviceCode + "-path:" + counter++} path={parsedRoute}
+                              options={{strokeColor: "#e076b4"}}/>
+                );
+            }
         }
 
-        setRoutePath(parsedRoutes);
-        setStopMarkers(parsedMarkers);
+        if (stopMarkers.length === 0) {
+            // Pre-calculate values, update prop after.
+            for (const point of mapData.StopLocations) {
+                const location: string[] = point.LatLng.split(",");
+                const stopCode: string = point.Sms;
+
+                parsedMarkers.push(
+                    <Marker
+                        position={{
+                            lat: parseFloat(location[0]),
+                            lng: parseFloat(location[1]),
+                        }}
+                        key={'stop-' + stopCode}
+                    />
+                );
+            }
+        }
+
+        // Set prop values at same time here to avoid reload between calculations.
+        if (routePath.length === 0) setRoutePath(parsedRoutes);
+        if (stopMarkers.length === 0) setStopMarkers(parsedMarkers);
     }
 
     getRouteData().then();
@@ -99,9 +106,12 @@ const MetlinkServiceView: FC<Props> = ({serviceCode}) => {
     return (
         <div>
             <p>Service Code: {serviceCode}</p>
+            {errorMessage && (
+                errorMessage
+            )}
             <GoogleMap
                 mapContainerStyle={mapContainerStyle}
-                zoom={8}
+                zoom={16}
                 center={center}
                 options={options}
                 onClick={event => {
