fa0aecec7ab06109c2cfe57968230286686f866d
Harrison <cookharr@ecs.vuw.ac.nz>
2020-08-18 21:47:23 +1200

    Implements infomation button to counteract google maps sizing issue

--- src/pages/ServicePerspective.tsx
+++ src/pages/ServicePerspective.tsx
@@ -1,10 +1,12 @@
-import React, {FC} from "react";
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import React, {FC, useState} from "react";
+import {IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from "@ionic/react";
 import MetlinkServiceView from "../components/metlink/service/MetlinkServiceView";
 import MetlinkServiceInfo from "../components/metlink/service/MetlinkServiceInfo";
 import BackButton from "../components/ui/BackButton";
+import {add, informationOutline, removeOutline} from "ionicons/icons";
 
 const ServicePerspective: FC<any> = ({match}) => {
+    const [showCard, setShowCard] = useState<boolean>(false)
     const {params: {serviceCode}} = match
 
     return (
@@ -16,13 +18,18 @@ const ServicePerspective: FC<any> = ({match}) => {
                 </IonToolbar>
             </IonHeader>
             <IonContent scrollX={false} scrollY={false}>
+                <IonFab vertical="bottom" horizontal={"start"} slot="fixed">
+                    <IonFabButton onClick={e => setShowCard(!showCard)} size={"small"}>
+                        <IonIcon icon={showCard ? removeOutline : informationOutline}/>
+                    </IonFabButton>
+                </IonFab>
                 <IonHeader collapse="condense">
                     <IonToolbar>
                         <IonTitle size="large">Map</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <MetlinkServiceView serviceCode={serviceCode}/>
-                <MetlinkServiceInfo serviceCode={serviceCode}/>
+                {showCard && (<MetlinkServiceInfo serviceCode={serviceCode}/>)}
             </IonContent>
         </IonPage>)
 }
