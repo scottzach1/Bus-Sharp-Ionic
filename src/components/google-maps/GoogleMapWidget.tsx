636e3c5726987130c8d5de9405130983d529d28f
Harrison <cookharr@ecs.vuw.ac.nz>
2020-08-18 20:31:55 +1200

    Fixes Google Map sizing issue
    Zac Scott's implementation redone here

--- src/components/google-maps/GoogleMapWidget.tsx
+++ src/components/google-maps/GoogleMapWidget.tsx
@@ -4,6 +4,7 @@ import {IonIcon} from "@ionic/react";
 import {locationSharp} from "ionicons/icons";
 import {readRemoteFile} from "react-papaparse";
 import "./GoogleMapWidget.css";
+import LoadingSpinner from "../ui/LoadingSpinner";
 
 interface Props {
     stopMarkers: StopMarker[] | null,
@@ -11,43 +12,40 @@ interface Props {
 }
 
 const libraries = ["places"];
-const mapContainerStyle = {
+
+
+/**
+ * Width and height of the google map.
+ */
+let mapContainerStyle = {
     width: '100vw',
     height: '100vh', // Need to figure out how to get 100% of max container height - no zoom!
 
 };
 
+/**
+ * Options for the map to allow for zoom and movement on the map.
+ */
 const options = {
     // disableDefaultUI: true, // Commented for street view.
     zoomControl: true,
     mapTypeControl: true,
 }
 
+/**
+ * Location where the map is centered
+ */
 let center = {
     lat: -41.286461,
     lng: 174.776230,
 }
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successfulPosition);
-    }
-    return center
-}
-
-function successfulPosition(position: any) {
-    console.log(position)
-    center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    }
-}
-
-
-
 const GoogleMapWidget: FC<Props> = ({stopMarkers, routePaths}) => {
     const [selectedStop, setSelectedStop] = useState<StopMarker | null>();
     const [stopData, setStopData] = useState<any | null>(null);
+    const [locationFound, setLocationFound] = useState<boolean>(false);
+
+    const [isReady, setIsReady] = useState<boolean>(false)
 
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -58,6 +56,9 @@ const GoogleMapWidget: FC<Props> = ({stopMarkers, routePaths}) => {
     if (loadError) return (<p>"Error: load error"</p>)
     if (!isLoaded) return (<p>"Error: not loaded"</p>)
 
+    /**
+     * Asynchronously get the stop data currently available.
+     */
     async function getStopData() {
         if (stopData) return;
         const proxy = "https://cors-anywhere.herokuapp.com/";
@@ -82,71 +83,98 @@ const GoogleMapWidget: FC<Props> = ({stopMarkers, routePaths}) => {
 
     }
 
-    getStopData().then();
+    /**
+     * Attempt to get the geo-location of the device accessing the map, and set the current position of the center
+     * of the map to this location.
+     */
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successfulPosition);
+        }
+        return center
+    }
 
+    /**
+     * Upon successfully getting the position of the user, set the center location of the map to this location.
+     * @param position - An object the holds the coordinates of the user.
+     */
+    function successfulPosition(position: any) {
+        center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+    }
+
+    /**
+     * Get the name of a marker on the map. Used when a marker has been clicked on.
+     * @param stop - A StopMarker
+     */
     function getStopName(stop: StopMarker) {
         if (stop.name) return stop.name;
         else if (stopData[stop.code]) return stopData[stop.code];
         else return stop.code + " - unknown";
     }
 
+    /**
+     * Get all the stop data asap.
+     */
+    getStopData().then();
+
     return (
-        <div className={"google-map-widget"}>
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                zoom={16}
-                center={getLocation()}
-                options={options}
-                onClick={event => {
-                    console.log(event);
-                }}
-            >
-                {stopMarkers?.map((marker) => (
-                    <Marker
-                        key={marker.key}
-                        position={{
-                            lat: marker.location.latitude,
-                            lng: marker.location.longitude,
-                        }}
-                        onClick={() => {
-                            setSelectedStop(marker);
-                        }}
-                    />
-                ))}
+        <GoogleMap
+            mapContainerClassName={"map"}
+            zoom={16}
+            center={getLocation()}
+            options={options}
+            onClick={event => {
+                console.log(event);
+            }}
+        >
+            {stopMarkers?.map((marker) => (
+                <Marker
+                    key={marker.key}
+                    position={{
+                        lat: marker.location.latitude,
+                        lng: marker.location.longitude,
+                    }}
+                    onClick={() => {
+                        setSelectedStop(marker);
+                    }}
+                />
+            ))}
 
-                {routePaths?.map((route) => (
-                    <Polyline
-                        key={route.key}
-                        path={route.path.map(position => ({
-                            lat: position.latitude,
-                            lng: position.longitude,
-                        }))}
-                        options={{strokeColor: route.color}}
-                    />
-                ))}
+            {routePaths?.map((route) => (
+                <Polyline
+                    key={route.key}
+                    path={route.path.map(position => ({
+                        lat: position.latitude,
+                        lng: position.longitude,
+                    }))}
+                    options={{strokeColor: route.color}}
+                />
+            ))}
 
-                {selectedStop && (
-                    <InfoWindow
-                        key={selectedStop.key + "-selected"}
-                        position={{
-                            lat: selectedStop.location.latitude,
-                            lng: selectedStop.location.longitude,
-                        }}
-                        onCloseClick={() => {
-                            setSelectedStop(null);
-                        }}
-                    >
-                        <div id="selected-stop-popup">
-                            <a href={'/stop/' + selectedStop.code}>
-                                <span><IonIcon icon={locationSharp}/></span>
-                                <strong>{getStopName(selectedStop)}</strong>
-                            </a>
-                        </div>
-                    </InfoWindow>
-                )}
+            {selectedStop && (
+                <InfoWindow
+                    key={selectedStop.key + "-selected"}
+                    position={{
+                        lat: selectedStop.location.latitude,
+                        lng: selectedStop.location.longitude,
+                    }}
+                    onCloseClick={() => {
+                        setSelectedStop(null);
+                    }}
+                >
+                    <div id="selected-stop-popup">
+                        <a href={'/stop/' + selectedStop.code}>
+                            <span><IonIcon icon={locationSharp}/></span>
+                            <strong>{getStopName(selectedStop)}</strong>
+                        </a>
+                    </div>
+                </InfoWindow>
+            )}
 
-            </GoogleMap>
-        </div>
+        </GoogleMap>
     )
 }
 
