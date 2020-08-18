57552e2315279658ee699077eef4b3896bd21d50
Zac Scott <scottzach1@myvuw.ac.nz>
2020-08-19 00:14:56 +1200

    Adds functionality to toggle between hours and time.
    In this commit, we add the functionality to toggle between hours and
    time. This is done by adding an optional prop to the ListComponent
    that contains time. This is rendered in non-bold on the line below
    the list entry. The user can select whether to show hours or time
    based off an IonToggle. (The entire IonItem is clickable, not just
    the toggle).

--- src/components/metlink/stop/MetlinkStopTable.tsx
+++ src/components/metlink/stop/MetlinkStopTable.tsx
@@ -1,5 +1,15 @@
 import React, {FC, useState} from 'react';
-import {IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList} from "@ionic/react";
+import {
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonToggle
+} from "@ionic/react";
 import LoadingSpinner from '../../ui/LoadingSpinner';
 import ListComponent from "../../ui/ListComponent";
 
@@ -8,8 +18,9 @@ interface Props {
 }
 
 const MetlinkStopTable: FC<Props> = ({stopCode}) => {
-    const [stopTimetableData, setStopTimetableData] = useState<any>()
-    const [errorMessage, setErrorMessage] = useState<string>()
+    const [stopTimetableData, setStopTimetableData] = useState<any>();
+    const [errorMessage, setErrorMessage] = useState<string>();
+    const [showHours, setShowHours] = useState<boolean>(false);
 
     const proxy = "https://cors-anywhere.herokuapp.com/";
     const url = 'https://www.metlink.org.nz/api/v1/StopDepartures/';
@@ -20,12 +31,23 @@ const MetlinkStopTable: FC<Props> = ({stopCode}) => {
         else Promise.resolve(resp.json()).then(data => setStopTimetableData(data));
     });
 
-    function getTimeRemaining(arrivalTime: string) {
+    function getHoursRemaining(arrivalTime: string) {
         const arrivalDate: Date = new Date(arrivalTime);
         const currentDate: Date = new Date();
 
         let timeRemaining: number | string = Math.round((arrivalDate.getTime() - currentDate.getTime()) / 60000);
         return (timeRemaining < 0) ? 'due' : timeRemaining + ' mins';
+    }
+
+    function getTime(arrivalTime: string) {
+        const arrivalDate: Date = new Date(arrivalTime);
+        const dateTimeFormat = new Intl.DateTimeFormat('en', {
+            month: 'short',
+            day: '2-digit',
+            hour: 'numeric',
+            minute: 'numeric'
+        })
+        return dateTimeFormat.format(arrivalDate);
     }
 
     function generateStopCards() {
@@ -40,15 +62,18 @@ const MetlinkStopTable: FC<Props> = ({stopCode}) => {
         let counter: number = 0;
         for (const service of services) {
             // Parse the time information from the response json.
-            const timeRemaining: string = getTimeRemaining(service.AimedArrival);
+
             const serviceName: string[] = service.Service.Name.split("-");
+
+            const timeRemaining: string = (showHours) ? getHoursRemaining(service.AimedArrival) : getTime(service.AimedArrival);
 
             cards.push(
                 <ListComponent
                     isStop={true}
                     code={service.ServiceID}
                     key={counter++ + ' - ' + serviceName[0]}
-                    title={serviceName[0] + ' - ' + timeRemaining}
+                    title={serviceName[0]}
+                    remaining={timeRemaining}
                     isLive={service.IsRealtime}
                 />
             )
@@ -66,7 +91,14 @@ const MetlinkStopTable: FC<Props> = ({stopCode}) => {
             <IonCard>
                 <IonCardHeader>
                     <IonCardTitle>Upcoming Services</IonCardTitle>
+                    <IonCardSubtitle>
+                        <IonItem onClick={() => setShowHours(!showHours)}>
+                            <IonLabel>Toggle Hours / Time</IonLabel>
+                            <IonToggle checked={showHours}/>
+                        </IonItem>
+                    </IonCardSubtitle>
                 </IonCardHeader>
+
                 <IonCardContent>
                     {stopTimetableData && (
                         generateStopCards()
