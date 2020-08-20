5264def890b7eb3a6a2e8ee94512018e1fd00c15
Zac Scott <scottzach1@myvuw.ac.nz>
2020-08-21 03:55:04 +1200

    Restructures Settings tab entries into components
    In this commit we restructure the settings
    tabs components into their respective
    component files.

--- src/pages/SettingsTab.tsx
+++ src/pages/SettingsTab.tsx
@@ -1,34 +1,10 @@
 import React, {Component} from "react";
-import {
-    IonAlert,
-    IonBackButton,
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonIcon,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonSelect,
-    IonSelectOption,
-    IonTitle,
-    IonToast,
-    IonToolbar
-} from "@ionic/react";
-import {Plugins} from '@capacitor/core';
-import {
-    codeSlashSharp,
-    colorPaletteSharp,
-    helpCircleSharp,
-    logoGitlab,
-    mailSharp,
-    shareSharp,
-    shareSocialSharp,
-    trashBinSharp
-} from "ionicons/icons";
-
-const {Storage, Share} = Plugins;
+import {IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import SettingsFeedbackItem from "../components/settings/SettingsFeedbackItem";
+import SettingsShareAppItem from "../components/settings/SettingsShareAppItem";
+import SettingsClearStorageItem from "../components/settings/SettingsClearStorageItem";
+import SettingsThemeSelector from "../components/settings/SettingsThemeSelector";
+import SettingsSourceCodeItem from "../components/settings/SettingsSourceCodeItem";
 
 interface Props {
 }
@@ -55,27 +31,6 @@ class SettingsTab extends Component<Props, State> {
         }
     }
 
-    componentDidMount() {
-        Storage.get({key: 'theme'}).then(res => {
-            if (res.value) this.setState({selectedTheme: res.value});
-        });
-    }
-
-    selectTheme(newTheme: string) {
-        Storage.set({key: 'theme', value: newTheme}).catch((e) => console.error(e));
-        this.setState({selectedTheme: newTheme});
-    }
-
-    clearStorage() {
-        Storage.clear()
-            .then(() => this.setState({showClearedToastSuccess: true}))
-            .catch(() => this.setState({showClearedToastFailure: true}));
-    }
-
-    // doShare() {
-    //
-    // }
-
     render() {
         return (
             <IonPage>
@@ -93,91 +48,11 @@ class SettingsTab extends Component<Props, State> {
                             <IonTitle size="large">Settings</IonTitle>
                         </IonToolbar>
                     </IonHeader>
-                    {/* CLEAR STORAGE */}
-                    <IonItem
-                        onClick={() => this.setState({showClearAlert: true})}
-                    >
-                        <IonIcon icon={trashBinSharp}/>
-                        <span>&nbsp;&nbsp;</span>
-                        <IonLabel>Clear Storage</IonLabel>
-                        <IonButton expand="block"><IonLabel>Clear</IonLabel></IonButton>
-                    </IonItem>
-                    <IonAlert
-                        isOpen={this.state.showClearAlert}
-                        onDidDismiss={() => this.setState({showClearAlert: false})}
-                        cssClass='my-custom-class'
-                        header={'Clear Storage'}
-                        subHeader={'Are you sure?'}
-                        message={'This will loose all user saved preferences such as saved stops, services and selected theme.'}
-                        buttons={[{text: 'Cancel', role: 'cancel'}, {
-                            text: 'Clear',
-                            handler: () => this.clearStorage()
-                        }]}
-                    />
-                    <IonToast
-                        isOpen={this.state.showClearedToastSuccess}
-                        onDidDismiss={() => this.setState({showClearedToastSuccess: false})}
-                        message={"Success"}
-                        duration={1200}
-                    />
-                    <IonToast
-                        isOpen={this.state.showClearedToastFailure}
-                        onDidDismiss={() => this.setState({showClearedToastFailure: false})}
-                        color={"danger"}
-                        message={"Failure"}
-                        duration={1200}
-                    />
-                    {/* THEME SELECTION */}
-                    <IonItem>
-                        <IonIcon icon={colorPaletteSharp}/>
-                        <span>&nbsp;&nbsp;</span>
-                        <IonLabel>Theme Selector</IonLabel>
-                        <IonSelect value={this.state.selectedTheme}
-                                   onIonChange={(e) => this.selectTheme(e.detail.value)}>
-                            <IonSelectOption value={"auto"}>Auto</IonSelectOption>
-                            <IonSelectOption value={"dark"}>Dark</IonSelectOption>
-                            <IonSelectOption value={"light"}>Light</IonSelectOption>
-                        </IonSelect>
-                    </IonItem>
-                    {/* SOURCE CODE */}
-                    <IonItem href={"https://gitlab.ecs.vuw.ac.nz/late-for-the-bus/bus-sharp"}>
-                        <IonIcon icon={logoGitlab}/>
-                        <span>&nbsp;&nbsp;</span>
-                        <IonLabel>View Source Code</IonLabel>
-                        <IonIcon icon={codeSlashSharp}/>
-                    </IonItem>
-                    {/* SHARE APP */}
-                    <IonItem onClick={() =>
-                        Share.share({
-                            title: 'Bus Sharp',
-                            text: 'The new way to track your public transport in Wellington, NZ.',
-                            url: 'https://welly.live',
-                            dialogTitle: 'Share with your buddies'
-                        }).catch(() =>
-                            // Failed to open share API, resort to clipboard share.
-                            navigator.clipboard.writeText('https://welly.live')
-                                .then(() => this.setState({showShareClipboardToast: true}))
-                                .catch((e) => console.error(e))
-                        )
-                    }>
-                        <IonIcon icon={shareSocialSharp}/>
-                        <span>&nbsp;&nbsp;</span>
-                        <IonLabel>Share</IonLabel>
-                        <IonIcon icon={shareSharp}/>
-                    </IonItem>
-                    <IonToast
-                        isOpen={this.state.showShareClipboardToast}
-                        onDidDismiss={() => this.setState({showShareClipboardToast: false})}
-                        message={"Copied to clipboard!"}
-                        duration={1200}
-                    />
-                    {/* FEEDBACK */}
-                    <IonItem href={"mailto:feedback@welly.live"}>
-                        <IonIcon icon={helpCircleSharp}/>
-                        <span>&nbsp;&nbsp;</span>
-                        <IonLabel>Help and Feedback</IonLabel>
-                        <IonIcon icon={mailSharp}/>
-                    </IonItem>
+                    <SettingsClearStorageItem/>
+                    <SettingsThemeSelector/>
+                    <SettingsSourceCodeItem/>
+                    <SettingsShareAppItem/>
+                    <SettingsFeedbackItem/>
                 </IonContent>
             </IonPage>
         );
