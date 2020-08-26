aca1955b7c4792baaf081f1f0ca0597671a00b62
Harrison <cookharr@ecs.vuw.ac.nz>
2020-08-27 01:19:05 +1200

    Fixes searchbar lag

--- src/pages/MapTab.tsx
+++ src/pages/MapTab.tsx
@@ -20,9 +20,8 @@ import {map} from "ionicons/icons";
 const libraries = ["places"];
 
 const MapTab: FC = () => {
-    const rerenderSearch = useRef<boolean>(false)
-    const [searchResults, setSearchResults] = useState<any>(null)
-    const searchText = useRef<string>("")
+    const searchResults = useRef<any>(null)
+    const [searchText, setSearchText] = useState<string>("")
     const [searchLocation, setSearchLocation] = useState<google.maps.GeocoderResult | undefined>(undefined)
 
     const {isLoaded, loadError} = useLoadScript({
@@ -30,24 +29,24 @@ const MapTab: FC = () => {
         libraries,
     })
 
-    function setSearch(event: CustomEvent) {
-        searchText.current = event.detail.value!
-        rerenderSearch.current = true
+    if (searchText) {
         let proxy = "https://cors-anywhere.herokuapp.com/";
         let urlBuilder = new URL("https://maps.googleapis.com/maps/api/place/autocomplete/json")
-        urlBuilder.searchParams.append("input", searchText.current)
+        urlBuilder.searchParams.append("input", searchText)
         urlBuilder.searchParams.append("types", "address")
         urlBuilder.searchParams.append("location", "-41.28646,174.77623")
         urlBuilder.searchParams.append("key", process.env.REACT_APP_GOOGLE_MAPS_API_KEY + "")
+        console.log(urlBuilder.href)
 
-        fetch(proxy + urlBuilder.href)
+        const response = async () => fetch(proxy + urlBuilder.href)
             .then(results => results.json())
             .then(data => {
-                rerenderSearch.current = false
                 if (data.predictions) {
-                    setSearchResults(data.predictions)
+                    console.log("HERE2")
+                    searchResults.current = data.predictions
                 }
             })
+        response()
     }
 
 
@@ -55,7 +54,7 @@ const MapTab: FC = () => {
         return (
             <IonList>
                 {
-                    searchResults.map((result: any) =>
+                    searchResults.current.map((result: any) =>
                         <IonItem key={result.description}>
                             {result.description}
                         </IonItem>
@@ -77,10 +76,17 @@ const MapTab: FC = () => {
             </IonHeader>
             <IonContent>
                 <MetlinkStopsMap geoCoderResult={searchLocation}/>
-                <IonSearchbar id={"searchbar"} value={searchText.current} onIonChange={e => searchText.current = e.detail.value!}/>
-                {searchResults && (
-                    <IonCard> {getPredictions()} </IonCard>
-                )}
+                <IonSearchbar id={"searchbar"} value={searchText} onIonChange={e => setSearchText(e.detail.value!)}/>
+                {searchText && (
+                    <IonCard>
+                        {searchResults.current && (
+                            getPredictions()
+                        )}
+                        {!searchResults.current && (
+                            <IonItem>No Results for "{searchText}"</IonItem>
+                        )}
+                    </IonCard>)
+                }
             </IonContent>
         </IonPage>
     );
