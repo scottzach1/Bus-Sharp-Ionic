ab5f12fa920363c98e01cda7a2cf078473bab787
Zac Scott <scottzach1@myvuw.ac.nz>
2020-08-27 03:40:49 +1200

    Fixes occasional cannot read property 'style' of undefined.

--- src/components/twitter/TwitterFeed.tsx
+++ src/components/twitter/TwitterFeed.tsx
@@ -11,14 +11,19 @@ const TwitterFeed = () => {
     // Keep listening until the element is rendered, before manipulating overflow and max-width property.
     async function styleFrame(maxAttempts: number, counter?: number) {
         if (document.getElementById('twitter-widget-0')) {
-            // Unfortunately using `.setAttribute` wont work here. So we have to manually manipulate the objects styles.
+            try {
+                // Unfortunately using `.setAttribute` wont work here. So we have to manually manipulate the objects styles.
 
-            // @ts-ignore - Add a scrollbar by manipulating the overflow property in DOM.
-            document.getElementsByClassName('twitter-embed')[0].style.overflow = 'auto';
+                // @ts-ignore - Add a scrollbar by manipulating the overflow property in DOM.
+                document.getElementsByClassName('twitter-embed')[0].style.overflow = 'auto';
 
-            let iframe = document.getElementById('twitter-widget-0')
-            // @ts-ignore - Remove content max-width by manipulating the maxWidth property in DOM.
-            iframe.contentWindow.document.getElementsByClassName('timeline-Widget')[0].style.maxWidth = 'none'
+                let iframe = document.getElementById('twitter-widget-0')
+                // @ts-ignore - Remove content max-width by manipulating the maxWidth property in DOM.
+                iframe.contentWindow.document.getElementsByClassName('timeline-Widget')[0].style.maxWidth = 'none'
+            } catch {
+                // Component wasn't actually ready, try again in a second.
+                setTimeout(() => styleFrame(maxAttempts, (counter) ? counter + 1 : 1), 500)
+            }
         } else if (counter && counter >= maxAttempts) return;
         else setTimeout(() => styleFrame(maxAttempts, (counter) ? counter + 1 : 1), 500);
     }
